f44170ddf29de8d7ba58de7680c483f8d16bdd88
=ulisses <ulissesnewdev@gmail.com>
2022-07-02 18:05:50 -0400

    feat: shared  module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
