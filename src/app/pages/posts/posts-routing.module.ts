8e1634138062bd67f7d4a4228254f4eaedd6fb16
=ulisses <ulissesnewdev@gmail.com>
2022-07-04 11:53:49 -0400

    feat: path id

--- src/app/pages/posts/posts-routing.module.ts
+++ src/app/pages/posts/posts-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PostsComponent
+  },
+  {
+    path: ':id',
+    component: PostComponent
   }
 ];
 
