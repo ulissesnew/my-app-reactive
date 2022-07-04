c7590e0e23a388896168de24bb644f66437e390b
=ulisses <ulissesnewdev@gmail.com>
2022-07-04 11:55:52 -0400

    feat: declarando PostComponent

--- src/app/pages/posts/posts.module.ts
+++ src/app/pages/posts/posts.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { PostComponent } from './post/post.component';
 
 
 @NgModule({
   declarations: [
-    PostsComponent
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     CommonModule,
