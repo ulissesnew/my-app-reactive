12e494e1e3c1f4b8276639b908b9a0cf54dadbe2
=ulisses <ulissesnewdev@gmail.com>
2022-07-07 10:51:34 -0400

    refactor: remove token

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { catchError, filter, map, tap } from 'rxjs/operators';
+import { catchError, delay, map, tap } from 'rxjs/operators';
 import { Observable, of, throwError } from 'rxjs';
 import { pluck } from 'rxjs/operators'
 import { POSTS } from './data/posts';
@@ -12,7 +12,6 @@ import { Post } from './models/posts';
 })
 export class PostsService {
   private posts$: Observable<{data: Post[]}> = of({data: POSTS})
-  token: any = 123;
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,36 +19,29 @@ export class PostsService {
 
   options = {
     headers: new HttpHeaders({
-      'Content-type': 'Application/json',
-      Authorization: `Bearer ${this.token}`
+      'Content-type': 'Application/json'
     })
   }
 
   getPosts(): Observable<Post[]> {
+    /* buscar dado real da internet 
+    * descomente a linha abaixo 
+    */
+    // return this.httpClient.get<Post[]>(`${this.apiUrl}/posts`, this.options)
     return this.posts$.pipe(
+      delay(1000),
       pluck('data'),
       // tap((x: any) => console.log('getPosts',x.length)),
       catchError((err) => {
         return throwError((() => new Error(`Algo deu errado! ${err}`)))
       })
     )
-    return this.httpClient.get<Post[]>(`${this.apiUrl}/posts`, this.options)
-    .pipe(
-      // tap((x: any) => console.log('getPosts',x.length)),
-      catchError((err) => {
-        return throwError((() => new Error(`Algo deu errado! ${err}`)))
-      })
-    )
+    
   }
 
   getPostId(id: number): Observable<Post> {
-    // return this.httpClient.get<Post>(`${this.apiUrl}/posts/${id}`, this.options)
-    // .pipe(
-    //   tap(console.log),
-    //   catchError((err) => {
-    //   return throwError(() => new Error(`Algo deu errado!, ${err}`))
-    //   })
-    // )
+    return this.httpClient.get<Post>(`${this.apiUrl}/posts/${id}`, this.options)
+   
     return this.posts$.pipe(
       pluck('data'),
       map((x) => {
