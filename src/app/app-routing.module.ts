df1001dd745e7f3627428afcc3bad681043e915d
=ulisses <ulissesnewdev@gmail.com>
2022-07-10 15:24:46 -0400

    feat: config page 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth.guard';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: 'posts',
     loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule),
     canLoad: [AuthGuard]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   },
   {
     path: '',
