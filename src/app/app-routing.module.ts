cca4e170ab494757f74963cb7baafe629ca93c2d
=ulisses <ulissesnewdev@gmail.com>
2022-07-15 10:40:19 -0400

    feat: passando data nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/auth.guard';
+import { ContactComponent } from './core/forms/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
     path: 'filtro-reativo',
-    loadChildren: () => import('./pages/filtro-reativo/filtro-reativo.module').then(m => m.FiltroReativoModule)
+    loadChildren: () => import('./pages/filtro-reativo/filtro-reativo.module').then(m => m.FiltroReativoModule),
+    data: {
+      title: "Filtro Reativo"
+    }
   },
   {
     path: 'posts',
     loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule),
-    canLoad: [AuthGuard]
+    canLoad: [AuthGuard],
+    data: {
+      title: "Posts"
+    }
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: {
+      title: "Contato"
+    }
   },
   {
     path: '**',
-    component: PageNotFoundComponent
+    component: PageNotFoundComponent,
+    data: {
+      title: "404"
+    }
   },
   {
     path: '',
