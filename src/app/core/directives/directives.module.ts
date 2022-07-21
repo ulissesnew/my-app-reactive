cd55a9f4f3485fb59f1f4756827cac836ae1c324
=ulisses <ulissesnewdev@gmail.com>
2022-07-21 14:06:45 -0400

    feat: nova  directive

--- src/app/core/directives/directives.module.ts
+++ src/app/core/directives/directives.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BetterHighligthDirective } from './better-highligth.directive';
+import { FormStatusDirective } from './form-status.directive';
 
 
 
 @NgModule({
   declarations: [
-    BetterHighligthDirective
+    BetterHighligthDirective,
+    FormStatusDirective
   ],
   imports: [
     CommonModule
   ], 
-  exports: [BetterHighligthDirective]
+  exports: [BetterHighligthDirective, FormStatusDirective]
 })
 export class DirectivesModule { }
